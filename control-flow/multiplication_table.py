c5324b61d56d577ec2786ed1ee181e536afc52ad
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-06-30 23:39:53 +0300

    python script to generate multiplication table for user's input number

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = int(input("Enter a number to see its multiplocation table:"))
+number = int(input("Enter a number to see its multiplication table:"))
 for i in range (1, 10+1):
     product = number * i
     print(number,"*",i,"=",product)
