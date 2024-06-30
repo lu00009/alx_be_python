5b7e860059579bdd58fa02ee5ee209add6b458f6
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-06-30 21:13:01 +0300

    Python script for basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = 10 + 5
+substruction = 10 - 5
+multiplication = 10 * 5
+print("Addition of 10 and 5 is",addition)
+print("Substruction of 10 and 5 is",substruction)
+print("Multiplication of 10 and 5 is",multiplication)
