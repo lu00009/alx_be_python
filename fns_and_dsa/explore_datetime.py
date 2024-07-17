7b1aa958250d4964d2df6563c76a78f8ef1be70c
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-07-17 12:23:47 +0300

    python script that explores current and future date and time

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,12 @@
-import datetime
+from datetime import datetime, timedelta
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time is: {formatted_datetime}")
-
+num_days = int(input("Enter a number of days: "))
 def calculate_future_date():
-    num_days = int(input("Enter a number of days: "))
-    current_date = datetime.datetime.now()
-    future_date = current_date + datetime.timedelta(days=num_days)
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=num_days)
     formatted_datetime = future_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time is: {formatted_datetime}")
 def main():
