74d09b321ad3b092ca265e24bffb468e1de66ae5
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-07-17 20:28:33 +0300

    python script that converts temp to celsius or fahreint

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,10 +2,12 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
 def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
     fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
     return fahrenheit
 
