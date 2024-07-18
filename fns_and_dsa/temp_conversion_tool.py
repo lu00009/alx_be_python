0827b1ef050414bf44519d6bcbd4ce25a48f01db
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-07-18 13:15:46 +0300

    python script that converts temp to celsius or fahreint

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,31 +1,28 @@
-
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-    return celsius
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    
-    fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
-    return fahrenheit
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
     try:
-        temp = float(input("Enter the temperature to convert: "))
-        temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
-    
-        if temp_type == 'C':
-            fahrenheit = convert_to_fahrenheit(temp)
-            print(f"{temp}°C is {fahrenheit:.2f}°F")
-        elif temp_type == 'F':
-            celsius = convert_to_celsius(temp)
-            print(f"{temp}°F is {celsius:.2f}°C")
-        else:
-            print("Invalid input. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
-    
+        temperature = float(input("Enter the temperature to convert: "))
     except ValueError:
-        print("Invalid temperature. Please enter a numeric value.")
+        raise ValueError("Invalid temperature. Please enter a numeric value.")
+    
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+    
+    if unit == 'C':
+        converted_temp = convert_to_fahrenheit(temperature)
+        print(f"{temperature}°C is {converted_temp}°F")
+    elif unit == 'F':
+        converted_temp = convert_to_celsius(temperature)
+        print(f"{temperature}°F is {converted_temp}°C")
+    else:
+        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+
 if __name__ == "__main__":
     main()
