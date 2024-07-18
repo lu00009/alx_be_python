69620675957701b5c8dd1698ca89d10a97148374
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-07-18 10:40:23 +0300

    python script that converts temp to celsius or fahreint

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,11 +3,12 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
 def convert_to_fahrenheit(celsius):
-    
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
     fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
     return fahrenheit
 
@@ -20,7 +21,7 @@ def main():
                 raise ValueError("Invalid unit.please enter C or F.")
             if temp_type == 'C':
                 fahrenheit = convert_to_fahrenheit(temp)
-                print(f"{temp}°C is {fahrenheit:.13f}°F")
+                print(f"{temp}°C is {fahrenheit:.1f}°F")
             else:
                 celsius = convert_to_celsius(temp)
                 print(f"{temp}°F is {celsius:.13f}°C")
