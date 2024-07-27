1285d7cd8c157039d50ce01b1dec6b13e1604ad6
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-07-27 21:17:06 +0300

    python script for simple bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,12 +4,11 @@ class BankAccount:
   def deposit (self,amount):
     if amount > 0:
         self.account_balance += amount
-  def withdraw (self,amount):
-    if 0<self.account_balance <= 0:
+  def withdraw(self, amount):
+    if amount <= self.account_balance:
       self.account_balance -= amount
-      print(f"withdrew:{self.account_balance}")
-    
-      
+      return True
+    return False
   def display_balance(self):
     print(f"Current Balance: ${self.account_balance:.2f}")
 
