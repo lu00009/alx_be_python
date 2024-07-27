749ec453d1d0155e6ff25b4bc7f2d2e801796573
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-07-27 20:57:46 +0300

    python script for simple bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,10 +7,9 @@ class BankAccount:
   def withdraw (self,amount):
     if 0<self.account_balance <= 0:
       self.account_balance -= amount
-      return True
-    else:
-      print(f"Insufficient funds.")
-      return False
+    
+    
+      
   def display_balance(self):
     print(f"Current Balance: ${self.account_balance:.2f}")
 
