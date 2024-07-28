252057d8143b267a4d5258cbf2b6abf83bc1a20f
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-07-28 10:56:51 +0300

    writing unit tests for a simple calculator

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,11 +4,13 @@ class BankAccount:
   def deposit (self,amount):
     if amount > 0:
         self.account_balance += amount
-  def withdraw(self, amount):
-    if amount <= self.account_balance:
+  def withdraw (self,amount):
+    if 0<self.account_balance <= 0:
       self.account_balance -= amount
-      return True
-    return False
+    
+    else:
+      print(f"Insufficient funds.")
+      
   def display_balance(self):
     print(f"Current Balance: ${self.account_balance:.2f}")
 
