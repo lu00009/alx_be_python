113e05dc025ef38983a99b3b5855ca1e2035cfdd
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-07-28 10:52:37 +0300

    Writing Unit Tests for a Simple Calculator Class

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,27 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+    def test_substraction(self):
+        self.assertEqual(self.calc.subtract(3, 1), 2)
+        self.assertEqual(self.calc.subtract(-3, 1), -4)
+        self.assertEqual(self.calc.subtract(-4, -1), -3)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(3, 0), 0)
+        self.assertEqual(self.calc.multiply(-3, 5), -15)
+        self.assertEqual(self.calc.multiply(-4, -3), 12)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(3, 0), None)
+# Remember to write additional test methods for subtract, multiply, and divide.
